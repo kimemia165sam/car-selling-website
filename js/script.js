6306dfaff5f69a84f3601bdd389d38ce424af38c
kimemia165sam <109588380+kimemia165sam@users.noreply.github.com>
2022-12-01 16:12:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
-    navbar.classList.toggle('ative');
+    navbar.classList.toggle('ative');   
 }
+
 document.querySelector('#login-btn').onclick = () =>{
     document.querySelector('.login-form-container').classList.toggle('active'); 
     
